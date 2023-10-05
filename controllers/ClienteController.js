6ace64f3ac0e63a2245eb3d95f8db83c4af5357d
Gabriel Meira <gabriel_meira10@hotmail.com>
2023-10-05 17:39:26 -0300

    not complete login controller

--- controllers/ClienteController.js
+++ controllers/ClienteController.js
@@ -1,5 +1,14 @@
-function login(req, res) {
-    res.send("Login funcionando")
+async function login(req, res) {
+    const email = req.body.email
+    const password = req.body.password
+
+    if(!email){
+        res.status(422).json({msg: "O email é obrigatório"})
+    }
+
+    if(!password || password.length < 5){
+        res.status(422).json({msg: "Senha inválida"})
+    }
 }
 
 module.exports = { login }
