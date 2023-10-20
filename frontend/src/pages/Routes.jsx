e825d85a0f2fc88145535bd81ea80e7a00bb95d6
HeitorMorais <heitormorais2003@gmail.com>
2023-10-20 10:49:38 -0300

    Adicionando página de registro

--- frontend/src/pages/Routes.jsx
+++ frontend/src/pages/Routes.jsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 
 import { Login } from "./Login.jsx";
+import { Registro } from "./Registro.jsx";
 
 export function AppRoutes(){
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<Login />}/>
+                <Route path="/registro" element={<Registro />}/>
             </Routes>
         </Router>
     )
