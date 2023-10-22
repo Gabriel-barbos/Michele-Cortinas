eb63be415416a0d642a99f27efc8d9d1199589a8
HenriqFonseca <henrique12095@gmail.com>
2023-10-22 20:42:17 -0300

    fix: CRUD verificatios

--- server/controllers/ClienteController.js
+++ server/controllers/ClienteController.js
@@ -1,56 +1,67 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
-const Cliente = require('../models').cliente
+const Cliente = require("../models").cliente;
 
 const login = async (req, res) => {
-    const email = req.body.email
-    const senha = req.body.senha
+  const email = req.body.email;
+  const senha = req.body.senha;
 
   if (!email) {
     res.status(422).json({ msg: "O email é obrigatório" });
   }
 
-    if(!senha || senha.length < 5){
-        res.status(422).json({msg: "senha inválida"})
-    }
+  if (!senha || senha.length < 5) {
+    res.status(422).json({ msg: "senha inválida" });
+  }
 
-    const cliente = await Cliente.findOne({ where: { email: email } })
-    if(!cliente) { res.status(500).json({msg: "Cliente não encontrado"}) }
+  const cliente = await Cliente.findOne({ where: { email: email } });
+  if (!cliente) {
+    res.status(500).json({ msg: "Cliente não encontrado" });
+  }
 
+  const checkSenha = await bcrypt.compare(senha, cliente.senha);
+  if (!checkSenha) {
+    res.status(422).json({ msg: "Senha incorreta" });
+  }
 
-    const checkSenha = await bcrypt.compare(senha, cliente.senha);
-    if(!checkSenha) { res.status(422).json({msg: "Senha incorreta"}) }
+  try {
+    const secret = process.env.JWT_SECRET;
+    const token = jwt.sign(
+      {
+        id: cliente.id,
+      },
+      secret
+    );
 
-    try {
-        const secret = process.env.JWT_SECRET
-        const token = jwt.sign({
-            id: cliente.id
-        }, secret)
-
-        res.status(200).json({msg: "Autenticação efetuada com sucesso", token})
-    } catch(err) {
-        console.log(err)
-        res.status(500).json({msg: "Ocorreu um erro na autenticação"})
-    }
-
-}
+    res.status(200).json({ msg: "Autenticação efetuada com sucesso", token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Ocorreu um erro na autenticação" });
+  }
+};
 
 // Create
-const register = async (req, res)=>{
+const register = async (req, res) => {
+  let info = {
+    nome: req.body.nome,
+    sobrenome: req.body.sobrenome,
+    email: req.body.email,
+    senha: await bcrypt.hash(req.body.senha, 10),
+  };
 
-    let info = {
-        nome: req.body.nome,
-        sobrenome: req.body.sobrenome,
-        email: req.body.email,
-        senha: await bcrypt.hash(req.body.senha, 10),
-    }
+  const emailIsRegistered = await Cliente.findOne({
+    where: { email: info.email },
+  });
+  if (emailIsRegistered) {
+    return res.status(422).json({ msg: "E-mail já cadastrado" });
+  }
 
-    const cliente = await Cliente.create(info)
-    res.status(200).send(cliente)
-}
+  const cliente = await Cliente.create(info);
+  res.status(200).send(cliente);
+};
 
 // Pegar todos os clientes
 
@@ -61,31 +72,43 @@ const getAllCliente = async (req, res) => {
 
 // Pegar um cliente
 const getOneCliente = async (req, res) => {
-  let id = req.params.id;
-  let cliente = await Cliente.findOne({ where: { id: id } });
-  res.status(200).send(cliente);
+  try {
+    let id = req.params.id;
+    let cliente = await Cliente.findOne({ where: { id: id } });
+    res.status(200).send(cliente);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 // Update Cliente
 const updateCliente = async (req, res) => {
-  let id = req.params.id;
+  try {
+    let id = req.params.id;
 
-  const cliente = await Cliente.update(req.body, { where: { id: id } });
-  res.status(200).send(cliente);
+    const cliente = await Cliente.update(req.body, { where: { id: id } });
+    res.status(200).send(cliente);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 };
 
 // delete cliente por id
-const deleteCliente = async (req, res)=>{
-    let id = req.params.id
-    await Cliente.destroy({ where: { id: id}})
-    res.status(200).send("produtos deletados")
-}
+const deleteCliente = async (req, res) => {
+  try {
+    let id = req.params.id;
+    await Cliente.destroy({ where: { id: id } });
+    res.status(200).send("produtos deletados");
+  } catch (error) {
+    res.status(400).json({error});
+  }
+};
 
-module.exports ={
-    login,
-    register,
-    getAllCliente,
-    getOneCliente,
-    updateCliente,
-    deleteCliente
-}
+module.exports = {
+  login,
+  register,
+  getAllCliente,
+  getOneCliente,
+  updateCliente,
+  deleteCliente,
+};
