5bd19b500861bd3dfd514d120701a955bbee849e
HenriqFonseca <henrique12095@gmail.com>
2023-11-02 13:36:56 -0300

    feat: OnDelete produto cascade to images

--- server/models/index.js
+++ server/models/index.js
@@ -2,20 +2,22 @@ const { sequelize, Sequelize } = require("../config/db");
 
 const models = {};
 
-models.cliente = require('./Cliente.js')
-models.pedido = require('./Pedido.js')
-models.produto = require('./Produto.js')
-models.imagem = require('./Imagem.js')
-models.categoria = require('./Categoria.js')
+models.cliente = require("./Cliente.js");
+models.pedido = require("./Pedido.js");
+models.produto = require("./Produto.js");
+models.imagem = require("./Imagem.js");
+models.categoria = require("./Categoria.js");
 
 //* Relacionamento Cliente x Pedido
 models.cliente.hasMany(models.pedido); //Um Cliente pode ter vários Pedidos
 models.pedido.belongsTo(models.cliente); // Cada Pedido está relacionado a um Cliente
 
 //* Relacionamento Imagem x Produto
-models.produto.hasMany(models.imagem)
-models.imagem.belongsTo(models.produto)
+models.produto.hasMany(models.imagem, {
+  onDelete: "cascade",
+});
+models.imagem.belongsTo(models.produto);
 
-sequelize.sync({ force: false });
+sequelize.sync({ force: true });
 
-module.exports = models
+module.exports = models;
