760e8073af615420ae79eea5122c2632e96ba648
HenriqFonseca <henrique12095@gmail.com>
2023-11-16 21:35:41 -0300

    feat: CRUD Variacao de produtos

--- server/models/index.js
+++ server/models/index.js
@@ -8,6 +8,7 @@ models.variacao = require("./Variacao.js");
 models.produto = require("./Produto.js");
 models.imagem = require("./Imagem.js");
 models.categoria = require("./Categoria.js");
+models.variacao = require("./Variacao.js");
 
 //* Relacionamento Cliente x Pedido
 models.cliente.hasMany(models.pedido); //Um Cliente pode ter vários Pedidos
@@ -19,10 +20,15 @@ models.produto.hasMany(models.imagem, {
 });
 models.imagem.belongsTo(models.produto);
 
-//* Relacionamento Imagem x Produto
+//* Relacionamento Variação de produto x Produto
 models.produto.hasMany(models.variacao)
 models.variacao.belongsTo(models.produto)
 
+//* Relacionamento Imagem x Variação de produto
+models.variacao.hasMany(models.imagem, {
+  onDelete: "cascade",
+});
+models.imagem.belongsTo(models.variacao);
 sequelize.sync({ force: true });
 
 module.exports = models;
--- server/server.js
+++ server/server.js
@@ -16,11 +16,13 @@ const clienteRoute = require("./routes/ClienteRouter")
 const produtoRoute = require("./routes/ProdutoRouter")
 const categoriaRoute = require("./routes/CategoriaRouter")
 const imagemRoute = require("./routes/ImagemRouter")
+const variacaoRoute = require("./routes/VariacaoRouter")
 
 app.use("/cliente", clienteRoute)
 app.use("/produto", produtoRoute)
 app.use("/categoria", categoriaRoute)
 app.use("/imagem", imagemRoute)
+app.use("/variacao", variacaoRoute)
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`🚀 Aplicação rodando: http://localhost:${process.env.APP_PORT}`)
