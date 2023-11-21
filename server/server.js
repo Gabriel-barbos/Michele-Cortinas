d11cf3432ddd10754142616acf0ec25981b087a1
HenriqFonseca <henrique12095@gmail.com>
2023-11-21 15:34:16 -0300

    feat: deleteOneImage function and images in updateProduto

--- server/server.js
+++ server/server.js
@@ -17,13 +17,11 @@ app.use(express.static('public'))
 const clienteRoute = require("./routes/ClienteRouter")
 const produtoRoute = require("./routes/ProdutoRouter")
 const categoriaRoute = require("./routes/CategoriaRouter")
-const imagemRoute = require("./routes/ImagemRouter")
 const variacaoRoute = require("./routes/VariacaoRouter")
 
 app.use("/cliente", clienteRoute)
 app.use("/produto", produtoRoute)
 app.use("/categoria", categoriaRoute)
-app.use("/imagem", imagemRoute)
 app.use("/variacao", variacaoRoute)
 
 app.listen(process.env.APP_PORT, () => {
